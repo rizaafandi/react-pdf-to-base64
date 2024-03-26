bb7630267397fd2156719ee5b7696346f51a4b98
Riza Afandi <riza.afandi@adaro.com>
2024-03-26 23:57:51 +0700

    feat: fix typos

--- src/App.tsx
+++ src/App.tsx
@@ -137,9 +137,9 @@ function App() {
               <button
                 onClick={downloadBase64Text}
                 type="button"
-                className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
-                Convery Base64 Text
+                Convert Base64 Text
               </button>
             </>
           ) : (
